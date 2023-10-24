1e1932106162636f8cbd9466266cfc76301d5029
Wendesday36 <bertazoeanna@gmail.com>
2023-10-24 10:13:41 +0200

    fejlec

--- View/AdatView.js
+++ View/AdatView.js
@@ -1,19 +1,29 @@
 import TablazatSorView from "./TablazatSorView.js";
-class AdatView{
-    #lista = [];
+class AdatView {
+  #lista = [];
 
-    constructor(lista, szuloElem) {
-      this.#lista = lista;
-  
-      szuloElem.append(`<table class= table >`);
-      this.tablaElem = szuloElem.children("table");
-  
-      this.megjelenit();
+  constructor(lista, szuloElem) {
+    this.#lista = lista;
+
+    szuloElem.append(`<table class= table >`);
+    this.tablaElem = szuloElem.children("table");
+
+    this.megjelenit();
+  }
+  megjelenit() {
+    this.fejlec()
+    this.#lista.forEach((elem, index) => {
+      new TablazatSorView(elem, this.tablaElem, index);
+    });
+  }
+  fejlec() {
+    let txt = "<tr>";
+    for (const key in this.#lista[0]) {
+      txt += `<th>${key}</th>`;
     }
-    megjelenit() {
-      this.#lista.forEach((elem,index) => {
-        new TablazatSorView(elem, this.tablaElem,index);
-      });
-    }
-    }
-    export default AdatView
+    txt += `</tr>`;
+
+    this.tablaElem.append(txt);
+  }
+}
+export default AdatView;
